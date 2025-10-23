8cb6b0a07a3ba6dd25da59696fd2e9e4506f30bb
Nafis Islam <nafis@kahf.co>
2025-10-23 12:25:53 +0600

    removed list sanity check from youtube url for testing

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "5.3.0"),
-        .package(url: "https://github.com/Kahf-Browser/content-scope-scripts", revision: "65aef831a497d6909722fc7ea2cc2702fe4745bd"),
+        .package(url: "https://github.com/Kahf-Browser/content-scope-scripts", revision: "1c350d08b493f708605680db666baf8d386d9b82"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
         .package(url: "https://github.com/1024jp/GzipSwift.git", exact: "6.0.1")
