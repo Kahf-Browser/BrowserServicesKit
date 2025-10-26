fb0e10f8234eca4a03b27228d6d7571038f454bd
Nafis Islam <nafis@kahf.co>
2025-10-26 20:14:20 +0600

    use new variable to fix the issue

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.2.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/privacy-dashboard", exact: "5.3.0"),
-        .package(url: "https://github.com/Kahf-Browser/content-scope-scripts", revision: "3c08754c956358a952cf105a0e65608dadcf3b84"),
+        .package(url: "https://github.com/Kahf-Browser/content-scope-scripts", revision: "67be32fdf4296ec7caa07ed0a3b114882d60296f"),
         .package(url: "https://github.com/httpswift/swifter.git", exact: "1.5.0"),
         .package(url: "https://github.com/duckduckgo/bloom_cpp.git", exact: "3.0.0"),
         .package(url: "https://github.com/1024jp/GzipSwift.git", exact: "6.0.1")
